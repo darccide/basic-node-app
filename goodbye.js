5bb07ae36840b56593f8e464ade602ea1ca5f5db
Steven Thomson <steven.thomson88@gmail.com>
2018-09-05 23:59:41 +0900

    basic_node_app checkpoint: Completed

---
+++ goodbye.js
@@ -0,0 +1,15 @@
+exports.goodbyeEnglish = function() {
+  console.log("See you later");
+}
+
+exports.goodbyeJapanese = function() {
+  console.log("さようなら");
+}
+
+exports.goodbyeSpanish = function() {
+  console.log("adiós");
+}
+
+exports.goodbyeIcelandic = function() {
+  console.log("bless")
+}
---
+++ main.js
@@ -0,0 +1,26 @@
+const greetings = require("./greeting.js");
+const goodbyes = require("./goodbye.js");
+
+// "Hello"
+greetings.greetingEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+
+// "Hola"
+greetings.greetingSpanish();
+
+// "Halló"
+greetings.greetingIcelandic();
+
+// "See you later"
+goodbyes.goodbyeEnglish();
+
+// "さようなら"
+goodbyes.goodbyeJapanese();
+
+// "adiós"
+goodbyes.goodbyeSpanish();
+
+// "Bless"
+goodbyes.goodbyeIcelandic();
